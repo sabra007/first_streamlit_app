bac57fd0dcaa8b937c112cde8301e24267147612
Sargis Abrahamyan <31526195+sabra007@users.noreply.github.com>
2023-08-13 23:14:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,9 +51,10 @@ def get_fruit_load_list():
     my_cur.execute("SELECT * from fruit_load_list")
     return my_cur.fetchall()
 
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   dataframe(my_data_rows)
 
 
@@ -66,4 +67,5 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   bff = insert_row_sf(add_my_fruit)
+  my_cnx.close()
   streamlit.text(bff)
